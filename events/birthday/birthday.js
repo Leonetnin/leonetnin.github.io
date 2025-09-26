f710b50b177568e8f80b2ce201b75ca6387bb0d7
Leonetnin <106881615+Leonetnin@users.noreply.github.com>
2025-09-27 00:02:03 +0200

    Broken ahh but idc

--- events/birthday/birthday.js
+++ events/birthday/birthday.js
@@ -1,12 +1,12 @@
-let r = Math.floor(Math.random()*254)+1
-let g = Math.floor(Math.random()*254)+1
-let b = Math.floor(Math.random()*254)+1
+let r = [Math.floor(Math.random()*254)+1, 1]
+let g = [Math.floor(Math.random()*254)+1, 1]
+let b = [Math.floor(Math.random()*254)+1, 1]
 
 function draw() {
-    r = Math.floor(Math.random()*254)+1
-    g = Math.floor(Math.random()*254)+1
-    b = Math.floor(Math.random()*254)+1
-    document.body.style.backgroundColor= "rgb("+r+","+g+","+b+")";
+    r = (r[0]<256) ? ([r[0]+r[1], r[1]]) : ([r[0]-r[1], r[1]*-1])
+    g = (g[0]<256) ? ([g[0]+g[1], g[1]]) : ([g[0]-g[1], g[1]*-1])
+    b = (b[0]<256 && b[0]) ? ([b[0]+b[1], b[1]]) : ([b[0]-b[1], b[1]*-1])
+    document.body.style.backgroundColor= "rgb("+r[0]+","+g[0]+","+b[0]+")";
     requestAnimationFrame(draw)
 }
 
