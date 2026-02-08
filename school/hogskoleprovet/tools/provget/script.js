b6f3a8c9db00aeb3df49241f35c71041997ed5f4
Leonetnin <106881615+Leonetnin@users.noreply.github.com>
2026-02-08 14:28:22 +0100

    Basically nothing
    Want to sync pc with this

--- school/hogskoleprovet/tools/provget/script.js
+++ school/hogskoleprovet/tools/provget/script.js
@@ -2,9 +2,20 @@ let input=await fetch("test.txt").then((res)=>res.text())
 input=input.split("open('").filter((x)=>{return x.startsWith("pdf")})
 input.forEach((text, i, arr)=>{arr[i]=arr[i].split("#")[0]})
 let p = document.getElementById("data")
-for (let i=0; i<input.length; i++){
-    let link= document.createElement("a")
-    link.href="https://allarätt.nu/"+input[i]
-    link.text=input[i]
-    document.body.appendChild(link)
+let button=document.createElement("button")
+button.innerText="download"
+button.onclick=()=>{
+    for (let i=0; i<input.length; i++){
+        fetch("https://allarätt.nu/"+input[i])
+        .then(response => response.blob())
+        .then(blob => {
+                let link= document.createElement("a")
+                link.href=URL.createObjectURL(blob)
+                link.text=input[i]
+                link.download=input[i].slice(4)
+                document.body.appendChild(link)
+                link.click()
+            })
+    }
 }
+document.body.appendChild(button)
